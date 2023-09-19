6c056df4e300580b47da9e966e77555009a3b714
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-19 15:01:26 -0300

    Criando as rotas

--- projetozoo/src/App.jsx
+++ projetozoo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -6,7 +6,22 @@ function App() {
 
   return (
     <>
-      <h1>Olá Mundo</h1>
+      <Router>
+
+        <Routes>
+
+          <Route exact path="/" element={<Home />}></Route>
+
+          <Route path="/addanimal" element={<NewAnimal />}></Route>
+
+          <Route path="/animals" element={<Animals />}></Route>
+
+          <Route path="/about" element={<About />}></Route>
+
+          <Route path="/contact" element={<Contact />}></Route>
+
+        </Routes>
+      </Router>
     </>
   )
 }
