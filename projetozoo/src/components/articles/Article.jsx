f4fa925c9a99c69a590621f8487678f39635bd41
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-21 17:09:08 -0300

    if e else no className da div

--- projetozoo/src/components/articles/Article.jsx
+++ projetozoo/src/components/articles/Article.jsx
@@ -2,38 +2,59 @@ import styles from './Article.module.css';
 
 import { FaCat, FaFrog } from 'react-icons/fa'
 
-var classNames = require('classnames');
+
 
 function Article({ animalType, description, animals, aliment, habitat }) {
+    /*
+
+    var backgroundImage = window.document.querySelector('.articleImg')
+
+    function addClass(animalType) {
+        
+        if(animalType == "Felinos") {
+            backgroundImage.classList.add('felinos');
+    
+        } else if(animalType == "Répteis") {
+            backgroundImage.classList.add('repteis');
+        }
+    }
+    */
+
+
+
+
     return (
         <>
             <p className={styles.title}>
                 <h1>{animalType == "Felinos" && <FaCat />}
-                    {animalType == "Répteis" && <FaFrog />} {animalType}</h1>
+                    {animalType == "Répteis" && <FaFrog />} {animalType}
+                </h1>
             </p>
 
 
-            
-
-                <div className={styles.articleImg}>
 
 
-                    <article className={styles.articleText}>
-                        <h1>Descrição:</h1>
-                        <p>{description}</p>
+            <div className={(animalType == "Felinos" ? styles.felinos : (
+                animalType == "Répteis" ? styles.repteis : " "
+            ))}>
 
-                        <h1>Alimentação: </h1>
-                        <p>{aliment}</p>
 
-                        <h1>Animais da classe: </h1>
-                        <p>{animals}</p>
+                <article className={styles.articleText}>
+                    <h1>Descrição:</h1>
+                    <p>{description}</p>
 
-                        <h1>Habitat Natural:</h1>
-                        <p>{habitat}</p>
-                    </article>
+                    <h1>Alimentação: </h1>
+                    <p>{aliment}</p>
 
-                </div>
-            
+                    <h1>Animais da classe: </h1>
+                    <p>{animals}</p>
+
+                    <h1>Habitat Natural:</h1>
+                    <p>{habitat}</p>
+                </article>
+
+            </div>
+
         </>
     )
 }
