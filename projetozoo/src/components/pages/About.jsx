409d1375f2f48ec969fff9269e7f7751ba9e792c
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-21 09:05:42 -0300

    página Sobre

--- projetozoo/src/components/pages/About.jsx
+++ projetozoo/src/components/pages/About.jsx
@@ -1,16 +1,22 @@
 import NavBar from '../layoult/NavBar'
 import Footer from '../layoult/Footer'
-import TemplatePattern from './TemplatePattern.module.css'
+import styles from './TemplatePattern.module.css'
+
+import {FaReact} from 'react-icons/fa'
 
 function About() {
     return(
         <>
-            <NavBar />
-            <section className={styles.container}>
-                <h1>Sobre:</h1>
-                <p>O jardim zoológico é um lugar legal!</p>
-                <p>Bem legal</p>
-            </section>
+            <NavBar/>
+            <div className={styles.containerBody}>
+                <section className={styles.container}>
+                    <h1>Sobre:</h1>
+                    <p>Simulação de um Sistema de Gerenciamento de animais de um jardim zoológico.</p>
+                    <p>Feito em React <i>
+                        <FaReact />
+                    </i></p>
+                </section>
+            </div>
             <Footer />
         </>
     )
