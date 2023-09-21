a041ca67edab3aaf7d35069d5ce5fdec7f701e3a
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-21 15:38:18 -0300

    Update Article.jsx

--- projetozoo/src/components/articles/Article.jsx
+++ projetozoo/src/components/articles/Article.jsx
@@ -2,33 +2,38 @@ import styles from './Article.module.css';
 
 import { FaCat, FaFrog } from 'react-icons/fa'
 
+var classNames = require('classnames');
 
-
-function Article( {animalType, description, animals, aliment, habitat} ) {
-    return(
+function Article({ animalType, description, animals, aliment, habitat }) {
+    return (
         <>
-        <p className={styles.title}>
-            <h1>{animalType == "Felinos" && <FaCat />}
-                {animalType =="Répteis" && <FaFrog />} {animalType}</h1>
-        </p>
+            <p className={styles.title}>
+                <h1>{animalType == "Felinos" && <FaCat />}
+                    {animalType == "Répteis" && <FaFrog />} {animalType}</h1>
+            </p>
 
-        <div className={styles.articleImg}>
 
-            <article className={styles.articleText}>
-                <h1>Descrição:</h1>
-                <p>{description}</p>
-
-                <h1>Alimentação: </h1>
-                <p>{aliment}</p>
-
-                <h1>Animais da classe: </h1>
-                <p>{animals}</p>
-
-                <h1>Habitat Natural:</h1>
-                <p>{habitat}</p>
-            </article>
             
-        </div>
+
+                <div className={styles.articleImg}>
+
+
+                    <article className={styles.articleText}>
+                        <h1>Descrição:</h1>
+                        <p>{description}</p>
+
+                        <h1>Alimentação: </h1>
+                        <p>{aliment}</p>
+
+                        <h1>Animais da classe: </h1>
+                        <p>{animals}</p>
+
+                        <h1>Habitat Natural:</h1>
+                        <p>{habitat}</p>
+                    </article>
+
+                </div>
+            
         </>
     )
 }
