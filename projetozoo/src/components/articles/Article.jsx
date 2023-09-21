3bedd8450b1a317cc54e8423dc021ee8a717ca98
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-21 14:22:35 -0300

    Article dos felinos completo!

---
+++ projetozoo/src/components/articles/Article.jsx
@@ -0,0 +1,36 @@
+import styles from './Article.module.css';
+
+import { FaCat, FaFrog } from 'react-icons/fa'
+
+
+
+function Article( {animalType, description, animals, aliment, habitat} ) {
+    return(
+        <>
+        <p className={styles.title}>
+            <h1>{animalType == "Felinos" && <FaCat />}
+                {animalType =="Répteis" && <FaFrog />} {animalType}</h1>
+        </p>
+
+        <div className={styles.articleImg}>
+
+            <article className={styles.articleText}>
+                <h1>Descrição:</h1>
+                <p>{description}</p>
+
+                <h1>Alimentação: </h1>
+                <p>{aliment}</p>
+
+                <h1>Animais da classe: </h1>
+                <p>{animals}</p>
+
+                <h1>Habitat Natural:</h1>
+                <p>{habitat}</p>
+            </article>
+            
+        </div>
+        </>
+    )
+}
+
+export default Article;
--- projetozoo/src/components/pages/Home.jsx
+++ projetozoo/src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import NavBar from '../layoult/NavBar';
 import Footer from '../layoult/Footer';
 import Button from '../layoult/Button';
+import Article from '../articles/Article';
 
 import styles from './Home.module.css';
 import butterfly from '../../../imagens/butterfly-256px.png'
@@ -9,18 +10,37 @@ function Home() {
     return (
         <div>
             <NavBar />
-            
+
             <div className={styles.containerBody}>
                 <section className={styles.container}>
-                 <div className={styles.titleContainer}>
-                     <img src={butterfly} alt="Butterfly" />
-                     <h1>ZOO MANAGER</h1>
-                 </div>
-                
-                <div className={styles.botao}><Button texto="Cadastrar Animal" /></div>
+                    <div className={styles.titleContainer}>
+                        <img src={butterfly} alt="Butterfly" />
+                        <h1>ZOO MANAGER</h1>
+                    </div>
+
+                    <div className={styles.botao}><Button texto="Cadastrar Animal" /></div>
                 </section>
             </div>
 
+            <article className={styles.article}>
+                <Article
+                    animalType="Felinos"
+
+                    description="Os felinos fazem parte da grande família de mamíferos carnívoros, que vai desde o gato doméstico até o leão, o rei da selva. Todos apresentam garras longas e encurvadas e alguns deles possuem unhas retrateis, ou seja, quando o animal as encolhe, elas são envolvidas pelas dobras da pele. As patas da frente têm 5 dedos e as de trás 4. Todos são privilegiados de um ótimo olfato, audição aguda e a capacidade de enxergar muito bem durante a noite. Possuem mais de 500 músculos e mantém o controle sobre todos. Sua espinha é bastante flexível, o que lhes permite subir em árvores com facilidade."
+
+                    animals="Alguns exemplos de felinos são: Gato doméstico, Gato-leopardo asiático, Jaguatirica, Leão, Leopardo, Lince-ibérico, Onça-pintada, Leão da Montanha, Puma, Pantera Negra e Tigre"
+
+                    aliment="Felinos são carnívores, ou seja se alimentam da carne de outros animais como a Zebra, Girafa, Antílope, Gnu, Veados Javalis, Coelhos, pequenos mamíferos e pássaros, e até mesmo alguns répteis como o jacaré no caso da Onça-Pintada. Os gatos domésticos comumente se alimentam de ração."
+
+                    habitat="Cerrado Caatinga, Pantanal, Florestas Tropicais e Sub-Tropicais"
+
+                    
+                 />
+
+                 <Article 
+                    animalType="Répteis"
+                 />
+            </article>
             <Footer />
         </div>
     )
