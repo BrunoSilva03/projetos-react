86e705aac9109708452362824cd8b3852559e988
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-10-02 14:44:38 -0300

    Estilização inicial do formulário

--- projetozoo/src/components/pages/NewAnimal.jsx
+++ projetozoo/src/components/pages/NewAnimal.jsx
@@ -12,19 +12,21 @@ function NewAnimal() {
             <div className={styles.newAnimalBody}>
                 <div className={styles.newAnimal_Container}>
                     <h1>Cadastrar Novo Animal</h1>
-                    <section className={styles.sectionForm}>
-                        <form>
-                            <p><label htmlFor="name">Nome:</label> <input id="name" name="name" type="text" /></p>
+                    <section >
+                        <form className={styles.sectionForm}>
+                            <p><p><label htmlFor="name">Nome:</label></p> <input id="name" name="name" type="text" /></p>
 
-                            <p><label htmlFor="animalType">Tipo do Animal:</label> <select name="animalType" id="animalType"></select></p>
-
-
-                            <p><label htmlFor="description">Descrição:</label> <input id="description" name="description" type="text" /></p>
+                            <p><p><label htmlFor="animalType">Tipo do Animal:</label></p> <select name="animalType" id="animalType"></select></p>
 
 
-                            <p><label htmlFor="aliment">Alimentação:</label> <input id="aliment" name="aliment" type="text" /></p>
+                            <p><p><label htmlFor="description">Descrição:</label></p> <input id="description" name="description" type="text" /></p>
 
-                            <p><label htmlFor="habitat">Habitat:</label> <select name="habitat" id="habitat"></select></p>
+
+                            <p><p><label htmlFor="aliment">Alimentação:</label></p> <input id="aliment" name="aliment" type="text" /></p>
+
+                            <p><p><label htmlFor="habitat">Habitat:</label></p> <select name="habitat" id="habitat"></select></p>
+
+                            <p><div className={styles.areaButton}><input type="submit" className={styles.btnCadastrar} value="Cadastrar"/></div></p>
                         </form>
                     </section>
                 </div>
