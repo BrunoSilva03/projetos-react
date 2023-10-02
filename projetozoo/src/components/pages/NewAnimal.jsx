14a5e04b659069ace922d9c513a52fba1c08ad2a
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-10-02 15:13:03 -0300

    selects

--- projetozoo/src/components/pages/NewAnimal.jsx
+++ projetozoo/src/components/pages/NewAnimal.jsx
@@ -16,7 +16,19 @@ function NewAnimal() {
                         <form className={styles.sectionForm}>
                             <p><p><label htmlFor="name">Nome:</label></p> <input id="name" name="name" type="text" /></p>
 
-                            <p><p><label htmlFor="animalType">Tipo do Animal:</label></p> <select name="animalType" id="animalType"></select></p>
+                            <p>
+                                <p><label htmlFor="animalType">Tipo do Animal:</label>
+                                </p>
+                                <select name="animalType" id="animalType">
+                                    <option value="selecionar" selected >Selecionar...</option>
+                                    <option value="felino">Felino</option>
+                                    <option value="reptil">Réptil</option>
+                                    <option value="ave">Ave</option>
+                                    <option value="anfibio">Anfíbio</option>
+                                    <option value="peixe">Peixe</option>
+                                    <option value="other">Outros</option>
+                                </select>
+                            </p>
 
 
                             <p><p><label htmlFor="description">Descrição:</label></p> <input id="description" name="description" type="text" /></p>
@@ -24,9 +36,19 @@ function NewAnimal() {
 
                             <p><p><label htmlFor="aliment">Alimentação:</label></p> <input id="aliment" name="aliment" type="text" /></p>
 
-                            <p><p><label htmlFor="habitat">Habitat:</label></p> <select name="habitat" id="habitat"></select></p>
+                            <p><p><label htmlFor="habitat">Habitat:</label></p>
+                                <select name="habitat" id="habitat">
+                                    <option value="selecionar" selected>Selecionar...</option>
+                                    <option value="terra">Terra</option>
+                                    <option value="ar">Ar</option>
+                                    <option value="agua">Água</option>
+                                    <option value="aguaeterra">Água e Terra</option>
+                                    <option value="terraear">Terra e Ar</option>
+                                    <option value="aguaterraear">Água, Terra e Ar</option>
+                                </select>
+                            </p>
 
-                            <p><div className={styles.areaButton}><input type="submit" className={styles.btnCadastrar} value="Cadastrar"/></div></p>
+                            <p><div className={styles.areaButton}><input type="submit" className={styles.btnCadastrar} value="Cadastrar" /></div></p>
                         </form>
                     </section>
                 </div>
