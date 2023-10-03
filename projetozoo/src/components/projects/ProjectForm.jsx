905981b3e31171732e033192df3a64e9a73f0bb0
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-10-03 13:59:20 -0300

    form

---
+++ projetozoo/src/components/projects/ProjectForm.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+import styles from './ProjectForm.module.css';
+
+function ProjectForm() {
+    return(
+        <h1>ProjectForm</h1>
+    )
+}
+
+export default ProjectForm;
