ddf3b2f92fc7b5a9280cc0785639a4bda710a982
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-10-11 15:29:34 -0300

    Estilo card de animal pronto 3

--- projetozoo/src/components/projects/AnimalsCard.jsx
+++ projetozoo/src/components/projects/AnimalsCard.jsx
@@ -1,16 +1,23 @@
 import styles from './AnimalsCard.module.css'
 
+import {FaPen, FaTrash} from 'react-icons/fa'
+
 function AnimalsCard() {
     return (
         <>
             <div className={styles.card}>
-            <h1>Nome animal</h1>
+                <h1>Nome animal</h1>
                 <div className={styles.conteudoCard}>
                     <p>Tipo do animal:</p>
                     <p>Alimentação:</p>
                     <p>Habitat:</p>
                     <p>Descrição:</p>
+
                 </div>
+                <p className={styles.areaBotoes}>
+                    <button className={styles.buttonAlterarAnimal}>Alterar <FaPen /></button>
+                    <button className={styles.buttonRemoverAnimal}>Excluir <FaTrash /></button>
+                    </p>
             </div>
         </>
 
