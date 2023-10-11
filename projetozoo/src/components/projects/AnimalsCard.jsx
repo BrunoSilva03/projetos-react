000157c3c74536d93b79bda0ba9b0d11ee648f18
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-10-11 14:53:38 -0300

    Estilização AnimalsCard

--- projetozoo/src/components/projects/AnimalsCard.jsx
+++ projetozoo/src/components/projects/AnimalsCard.jsx
@@ -1,15 +1,19 @@
 import styles from './AnimalsCard.module.css'
 
 function AnimalsCard() {
-    return(
-        <div className={styles.card}>
+    return (
+        <>
+            <div className={styles.card}>
             <h1>Nome animal</h1>
-            <p>Tipo do animal</p>
-            <p>Alimentação</p>
-            <p>Habitat</p>
-            <p>Descrição</p>
-        </div>
-        
+                <div className={styles.conteudoCard}>
+                    <p>Tipo do animal:</p>
+                    <p>Alimentação:</p>
+                    <p>Habitat:</p>
+                    <p>Descrição:</p>
+                </div>
+            </div>
+        </>
+
     )
 }
 
