cd5d41de666a4b00558644b9bd4d60624c3358de
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-10-17 14:30:01 -0300

    Componente de Mensagens do Sistema

--- projetozoo/src/components/pages/AnimalsTest.jsx
+++ projetozoo/src/components/pages/AnimalsTest.jsx
@@ -1,8 +1,13 @@
+import Message from '../layoult/Message'
+
 import styles from './AnimalsTest.module.css'
 
 function AnimalTest() {
     return(
+        <>
         <h1>AnimalTest</h1>
+        <Message msg="Alguma mensagem" type="sucess"/>
+        </>
     )
 }
 
