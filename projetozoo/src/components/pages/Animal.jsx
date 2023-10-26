e9d6064f34c45224bcb3884c5b197fa5f023b686
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-10-26 16:02:45 -0300

    animalType e habitat passando corretamente

--- projetozoo/src/components/pages/Animal.jsx
+++ projetozoo/src/components/pages/Animal.jsx
@@ -1,6 +1,7 @@
 import NavBar from '../Layoult/NavBar'
 import Footer from '../Layoult/Footer'
 import Container from '../Layoult/Container'
+import AnimalCard from '../projects/AnimalsCard'
 
 import styles from './Animal.module.css'
 
@@ -12,6 +13,9 @@ function Animal() {
     
     const [animal, setAnimal] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [tipoAnimal, setTipoAnimal] = useState()
+    const [habitat, setHabitat] = useState()
+    
     
 
 
@@ -22,9 +26,18 @@ function Animal() {
                 'Content-Type': 'application/json',
             },
 
-        }).then((response) => response.json())
+        })
+          .then((response) => response.json())
           .then((data) => {
             setAnimal(data)
+            setTipoAnimal(data.animalType.name)
+            setHabitat(data.habitat.name)
+            const tipoAnimal = data.animalType.name
+            const habitat = data.habitat.name
+            console.log("animalType: " + data.animalType.name)
+            console.log(data)
+            console.log(animal)
+            console.log("description: " + data.description)
           })
           .catch((err) => console.log(err))
     }, [id])
@@ -32,6 +45,7 @@ function Animal() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+        console.log(animal)
     }
 
     return(
@@ -41,27 +55,30 @@ function Animal() {
         <NavBar />
         <div>
             <Container customClass="column">
-                <div>
+                <div className={styles.animal_details}>
                     <h1>Animal: {animal.idnome}</h1>
                     <button onClick={toggleProjectForm}>
                         {!showProjectForm ? 'Editar animal' : 'Fechar'}
                         </button>
                         {!showProjectForm ? (
                             <div>
-                                <p>
-                                <span>Habitat: </span> 
-                                   {/* {console.log('animalType: ' + animal.animalType.name)} */}
-                                </p>
-                                <p>
-                                    <span>Alimentação: </span> {animal.idaliment}
-                                </p>
-                                <p>
-                                    <span>Descrição: </span> {animal.description}
-                                </p>
-                                <p>
-                                <span>Tipo do animal: </span> 
-                                </p>
-                            </div>
+                            <p>
+                            <span>Tipo do animal: </span> {tipoAnimal}
+                           
+                               {/* {console.log('animalType: ' + animal.animalType.name)} */}
+                            </p>
+                            <p>
+                                <span>Alimentação: </span> {animal.idaliment}
+                            </p>
+                            <p>
+                                <span>Descrição: </span> {animal.description}
+                            </p>
+                            <p>
+                            <span>Habitat: </span> {habitat}
+                            </p>
+                        </div>
+                            
+                           
                         ) : (
                             <div>
                                 <p>detalhes do projeto</p>
